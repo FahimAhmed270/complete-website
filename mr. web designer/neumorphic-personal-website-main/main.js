d9cd583b48b8c7fcd63bdb1f10c286f94a7bb886
FahimAhmed270 <56820604+FahimAhmed270@users.noreply.github.com>
2022-03-01 21:19:54 +0600

    neumorphic personal website

---
+++ mr. web designer/neumorphic-personal-website-main/main.js
@@ -0,0 +1,21 @@
+// menu button
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick=()=>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+// theme toggle
+
+let themeToggle = document.querySelector('#theme-toggle');
+
+themeToggle.onclick=()=>{
+    themeToggle.classList.toggle('fa-sun');
+    if(themeToggle.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
